e632b069665d8f67aa2a95f7b3083d232225a37c
EmilyYoung26 <emilyann101@live.co.uk>
2026-03-06 12:50:59 +0000

    Flattened project structure (move dbt project to repo root)

---
+++ target/compiled/spotify_analytics/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `emilys-analytics`.`spotify_analytics`.`my_first_dbt_model`
+where id is null
+
+
---
+++ target/compiled/spotify_analytics/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `emilys-analytics`.`spotify_analytics`.`my_second_dbt_model`
+where id is null
+
+
